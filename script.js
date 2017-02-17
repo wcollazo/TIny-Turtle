169b4122f5ff9eec7d366ea57cb5d6cd9bb0e893
William Collazo <wcollazo@cristoreyny.org>
2017-02-17 14:37:40 +0000

    This one is on github!

---
+++ script.js
@@ -0,0 +1,21 @@
+TinyTurtle.apply(window);
+function createdrill(width) {
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(230);
+forward(74);
+left(97);
+forward(76);
+}
+createdrill(100);
+
+stamp();
+
+
+
+stamp();
